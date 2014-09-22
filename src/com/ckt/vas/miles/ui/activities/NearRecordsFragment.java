5600add34da1840821c9b75168d41b0ef2398d5d
翟冰洁 <zhaibingjie@baixing.com>
2014-09-23 00:22:11 +0800

    正确获取周边信息。

--- src/com/ckt/vas/miles/ui/activities/NearRecordsFragment.java
+++ src/com/ckt/vas/miles/ui/activities/NearRecordsFragment.java
@@ -3,7 +3,9 @@ package com.ckt.vas.miles.ui.activities;
 import java.util.List;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,8 @@ import com.avos.avoscloud.AVGeoPoint;
 import com.avos.avoscloud.AVOSCloud;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
+import com.avos.avoscloud.FindCallback;
+import com.baixing.data.ChatMessage;
 import com.ckt.vas.miles.R;
 import com.ckt.vas.miles.dto.Constants;
 
@@ -36,38 +40,48 @@ public class NearRecordsFragment extends Fragment {
 
 			@Override
 			public void onClick(View v) {
-				jhLocateClient.stop();
-				jhLocateClient.start();
+				new findNearPeopleTask().execute();
 			}
         	
         });
         return rootView;
 	}
 	
-	String nearInfos = "";
-	private void findNearPeople() {
-		//定位
-		AVGeoPoint userLocation = new AVGeoPoint(31.1982322, 121.4348722);
-		AVQuery<AVObject> query = new AVQuery<AVObject>("LocationRecord");
-		query.whereNear("location", userLocation);
-		query.setLimit(10);            //获取最接近用户地点的10条数据
-		try {
-			List<AVObject> nearPlaces = query.find();
-			for (int i = 0; i < nearPlaces.size(); i++) {
-				AVObject object = nearPlaces.get(i);
+	private class findNearPeopleTask extends AsyncTask<Void, Void, List<AVObject>> {
+
+		@Override
+		protected List<AVObject> doInBackground(Void... params) {
+			//定位
+			AVGeoPoint userLocation = new AVGeoPoint(31.1982322, 121.4348722);
+			AVQuery<AVObject> query = new AVQuery<AVObject>("LocationRecord");
+			query.whereNear("location", userLocation);
+			query.setLimit(10);            //获取最接近用户地点的10条数据
+			try {
+				List<AVObject> nearPlaces = query.find();
+				return nearPlaces;
+			} catch (AVException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+		@Override
+		protected void onPostExecute(List<AVObject> result) {
+			if (result == null) {
+				return;
+			}
+			for (int i = 0; i < result.size(); i++) {
+				AVObject object = result.get(i);
 				nearInfos += object.getString("record");
 			}
-			locationTextView.setText(nearInfos);
-		} catch (AVException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			editText.setText(nearInfos);
 		}
 	}
+	String nearInfos = "";
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AVOSCloud.initialize(getActivity(), Constants.App_ID, Constants.App_KEY);
-		findNearPeople();
+		//findNearPeople();
 	}
 }
